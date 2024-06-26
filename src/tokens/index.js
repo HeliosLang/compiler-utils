70341b5966c80fcf5caca16db2961ec25125de8c
Christian Schmitz <cschmitz398@gmail.com>
2024-06-26 15:49:56 -0600

    correctly exported SourceMap

--- src/tokens/index.js
+++ src/tokens/index.js
@@ -19,7 +19,7 @@ export { TokenReader } from "./TokenReader.js"
 export { Word } from "./Word.js"
 
 /**
- * @typedef {import("./SourceMap.js").SourceMap}
+ * @typedef {import("./SourceMap.js").SourceMap} SourceMap
  * @typedef {import("./Token.js").Token} Token
  */
 
