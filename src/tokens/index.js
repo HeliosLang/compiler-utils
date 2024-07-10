b9a23981e8dc3a9ea44c7e183070a1a0b681fb12
Christian Schmitz <cschmitz398@gmail.com>
2024-07-10 07:59:43 -0600

    exported SymbolToken

--- src/tokens/index.js
+++ src/tokens/index.js
@@ -3,6 +3,7 @@ export { Group } from "./Group.js"
 export { REAL_PRECISION, RealLiteral } from "./RealLiteral.js"
 export { Source } from "./Source.js"
 export { StringLiteral } from "./StringLiteral.js"
+export { SymbolToken } from "./SymbolToken.js"
 export { Tokenizer } from "./Tokenizer.js"
 export {
     anySymbol,
